d62b53340f7813e4c73ab432f53c50891fb48a1b
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-03-01 18:21:24 +0545

    created a tensorflow layer of autoencoder

--- autoencoders/models.py
+++ autoencoders/models.py
@@ -5,41 +5,47 @@ import tensorflow.keras.backend as K
 
 import numpy as np 
 
-IMAGE_SIZE = 32
-CHANNELS = 1
-BATCH_SIZE = 100
-BUFFER_SIZE = 1000
-VALIDATION_SPLIT = 0.2
-EMBEDDING_DIM = 2
-EPOCHS = 3
+class Autoencoder(tf.keras.layers.Layer):
+    def __init__(self, image_size, channels, embedding_dim,  **kwargs):
+        super(Autoencoder, self).__init__(**kwargs)
+        self.image_size = image_size
+        self.channels = channels
+        self.embedding_dim = embedding_dim
 
-# Encoder
-encoder_input = Input(shape=(IMAGE_SIZE, IMAGE_SIZE, CHANNELS), name="encoder_input")
-x = Conv2D(32, (3, 3), strides=2, activation="relu", padding="same")(encoder_input)
-x = Conv2D(64, (3, 3), strides=2, activation="relu", padding="same")(x)
-x = Conv2D(128, (3, 3), strides=2, activation="relu", padding="same")(x)
-shape_before_flattening = K.int_shape(x)[1:]  # the decoder will need this!
+    def encoder(self):
+        # Encoder
+        self.encoder_input = Input(shape=(self.image_size,
+                                     self.image_size,
+                                     self.channels), name="encoder_input")
+        x = Conv2D(32, (3, 3), strides=2, activation="relu", padding="same")(self.encoder_input)
+        x = Conv2D(64, (3, 3), strides=2, activation="relu", padding="same")(x)
+        x = Conv2D(128, (3, 3), strides=2, activation="relu", padding="same")(x)
+        self.encoder_out_dim = K.int_shape(x)[1:]  # the decoder will need this!
 
-x = Flatten()(x)
-encoder_output = Dense(EMBEDDING_DIM, name="encoder_output")(x)
+        x = Flatten()(x)
+        encoder_output = Dense(self.encoder_out_dim, name="encoder_output")(x)
 
-encoder = Model(encoder_input, encoder_output)
-encoder.summary()
+        encoder = Model(self.encoder_input, encoder_output)
 
-# Decoder
-decoder_input = Input(shape=(EMBEDDING_DIM,), name="decoder_input")
-x = Dense(np.prod(shape_before_flattening))(decoder_input)
-x = Reshape(shape_before_flattening)(x)
-x = Conv2DTranspose(128, (3, 3), strides=2, activation="relu", padding="same")(x)
-x = Conv2DTranspose(64, (3, 3), strides=2, activation="relu", padding="same")(x)
-x = Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same")(x)
-decoder_output = Conv2D(
-    CHANNELS, (3, 3), strides=1, activation="sigmoid", padding="same", name="decoder_output"
-)(x)
+        return encoder
+    
+    def decoder(self):
+        # Decoder
+        decoder_input = Input(shape=(self.embedding_dim, ), name="decoder_input")
+        x = Dense(np.prod(self.encoder_out_dim))(decoder_input)
+        x = Reshape(self.encoder_out_dim)(x)
+        x = Conv2DTranspose(128, (3, 3), strides=2, activation="relu", padding="same")(x)
+        x = Conv2DTranspose(64, (3, 3), strides=2, activation="relu", padding="same")(x)
+        x = Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same")(x)
+        self.decoder_output = Conv2D(self.channels, (3, 3), strides=1, activation="sigmoid", padding="same", name="decoder_output"
+        )(x)
 
-decoder = Model(decoder_input, decoder_output)
-decoder.summary()
+        decoder = Model(decoder_input, self.decoder_output)
 
-# Autoencoder
-autoencoder = Model(encoder_input, decoder(encoder_output))  # decoder(encoder_output)
-autoencoder.summary()
+        return decoder
+
+    def call(self):
+        # Autoencoder
+        autoencoder = Model(self.encoder_input, self.decoder(self.encoder_output))  # decoder(encoder_output)
+        
+        return autoencoder
