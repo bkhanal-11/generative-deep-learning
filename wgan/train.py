9888a11d1d131d43fc5ac4dfbbd9b32843cca887
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-03-27 16:35:12 +0545

    added training code

---
+++ wgan/train.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
+
+from models import Generator, Critic
+from utils import *
+
+# Define device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Set your parameters
+n_epochs = 100
+z_dim = 64
+display_step = 500
+batch_size = 128
+learning_rate = 0.0002
+
+beta_1 = 0.5
+beta_2 = 0.999
+
+c_lambda = 10
+crit_repeats = 5
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+
+# Load MNIST dataset
+train_set = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+# Load Model
+gen = Generator(z_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
+
+crit = Critic().to(device) 
+crit_opt = torch.optim.Adam(crit.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+
+gen = gen.apply(weights_init)
+crit = crit.apply(weights_init)
+
+cur_step = 0
+visualization = False
+
+generator_losses = []
+critic_losses = []
+for epoch in range(n_epochs):
+    # Dataloader returns the batches
+    for real, _ in tqdm(train_loader):
+        cur_batch_size = len(real)
+        real = real.to(device)
+
+        mean_iteration_critic_loss = 0
+        for _ in range(crit_repeats):
+            ### Update critic ###
+            crit_opt.zero_grad()
+            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+            fake = gen(fake_noise)
+            crit_fake_pred = crit(fake.detach())
+            crit_real_pred = crit(real)
+
+            epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
+            gradient = get_gradient(crit, real, fake.detach(), epsilon)
+            gp = gradient_penalty(gradient)
+            crit_loss = get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda)
+
+            # Keep track of the average critic loss in this batch
+            mean_iteration_critic_loss += crit_loss.item() / crit_repeats
+            # Update gradients
+            crit_loss.backward(retain_graph=True)
+            # Update optimizer
+            crit_opt.step()
+        critic_losses += [mean_iteration_critic_loss]
+
+        ### Update generator ###
+        gen_opt.zero_grad()
+        fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+        fake_2 = gen(fake_noise_2)
+        crit_fake_pred = crit(fake_2)
+        
+        gen_loss = get_gen_loss(crit_fake_pred)
+        gen_loss.backward()
+
+        # Update the weights
+        gen_opt.step()
+
+        # Keep track of the average generator loss
+        generator_losses += [gen_loss.item()]
+
+        ### Visualization code ###
+        if cur_step % display_step == 0 and cur_step > 0:
+            gen_mean = sum(generator_losses[-display_step:]) / display_step
+            crit_mean = sum(critic_losses[-display_step:]) / display_step
+            print(f"Epoch {epoch}, step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
+            if visualization:
+                show_tensor_images(fake)
+                show_tensor_images(real)
+            step_bins = 20
+            num_examples = (len(generator_losses) // step_bins) * step_bins
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Generator Loss"
+            )
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Critic Loss"
+            )
+            plt.legend()
+            plt.show()
+
+        cur_step += 1
